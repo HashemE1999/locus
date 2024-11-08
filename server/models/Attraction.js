2739a0016cf70789267fadd84d7be96f952fcdbb
otekolste <58699635+otekolste@users.noreply.github.com>
2024-11-07 20:25:35 -0500

    added date scalar to Attraction.js schema + typedef

--- server/models/Attraction.js
+++ server/models/Attraction.js
@@ -1,28 +1,32 @@
 // This model is based on the API documentation from the Points of Interest
 // https://developers.amadeus.com/self-service/category/destination-experiences/api-doc/points-of-interest/api-reference
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
 
 const AttractionSchema = new Schema({
   name: {
     type: String,
     required: true,
-    trim: true
+    trim: true,
+  },
+  date: {
+    type: Date,
+    required: true,
   },
   category: {
     type: String,
     required: true,
-    uppercase: true
+    uppercase: true,
   },
   rank: {
     type: Number,
     required: true,
-    min: 1
+    min: 1,
   },
   tags: {
     type: [String],
-    default: []
-  }
+    default: [],
+  },
 });
 
-module.exports = model('Attraction', AttractionSchema);
+module.exports = model("Attraction", AttractionSchema);
