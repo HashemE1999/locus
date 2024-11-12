1a3de901bf51aa03117e8b3ac27e46160cc9c0a3
Halcyon <hashem_elmaghrabi@yahoo.com>
2024-11-12 18:05:41 -0500

    More text fixes

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -51,7 +51,7 @@ const LoginForm = () => {
           <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
               <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                Create an account
+                Log In
               </h1>
               <form
                 novalidate
@@ -79,28 +79,6 @@ const LoginForm = () => {
                   />
                   <span class="mt-2 hidden text-sm text-red-500 peer-[&:not(:placeholder-shown):not(:focus):invalid]:block">
                     Please enter a valid email address
-                  </span>
-                </div>
-                <div>
-                  <label
-                    for="username"
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                  >
-                    Username
-                  </label>
-                  <input
-                    type="username"
-                    name="username"
-                    id="username"
-                    placeholder="username"
-                    className="peer invalid:[&:not(:placeholder-shown):not(:focus)]:border-red-500 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    required=""
-                    onChange={handleInputChange}
-                    value={userFormData.username}
-                    pattern=".{1,}"
-                  />
-                  <span class="mt-2 hidden text-sm text-red-500 peer-[&:not(:placeholder-shown):not(:focus):invalid]:block">
-                    Username cannot be blank
                   </span>
                 </div>
                 <div>
