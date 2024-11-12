cd2aab7e32931ed832408b9946bc4727e5aa9896
Halcyon <hashem_elmaghrabi@yahoo.com>
2024-11-12 17:48:29 -0500

    Finalized Navbar.jsx changes and implemented alternative LoginForm.jsx styling

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,87 +1,38 @@
-import { useState } from "react";
 import { Link } from "react-router-dom";
-import { Navbar, Nav, Container, Modal, Tab } from "react-bootstrap";
 
 import Auth from "../utils/auth";
-import LoginForm from "./LoginForm";
-import SignupForm from "./SignupForm";
 
 const AppNavbar = () => {
   // set modal display state
-  const [showModal, setShowModal] = useState(false);
 
   return (
     <>
-      <Navbar className="bg-white border-gray-200 dark:bg-gray-900">
-        <Container
+      <nav className="bg-white border-gray-200 dark:bg-gray-900">
+        <div
           fluid
           className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4"
         >
-          <Navbar.Brand as={Link} to="/">
-            Amadeus Locations Search
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="navbar" />
-          <Navbar.Collapse id="navbar" className="d-flex flex-row-reverse">
             <div className="ml-auto d-flex">
-              <Nav.Link as={Link} to="/">
-                Search For Locations
-              </Nav.Link>
-              {/* if user is logged in show saved books and logout */}
+            {/* if user is logged in show saved locations and logout */}
               {Auth.loggedIn() ? (
                 <>
-                  <Nav.Link as={Link} to="/saved">
-                    See Your Locations
-                  </Nav.Link>
-                  <Nav.Link onClick={Auth.logout}>Logout</Nav.Link>
+                  <Link to="/mytrips">
+                    See Your Trips
+                  </Link>
+                  <nav onClick={Auth.logout}>Logout</nav>
                 </>
-              ) : (
-                <Nav.Link onClick={() => setShowModal(true)}>
-                  Login/Sign Up
-                </Nav.Link>
-              )}
+                ) : (
+                <Link to="/login">
+                  Login
+                </Link>
+                )}
+                <Link to="/signup">
+                  Signup
+                </Link>
             </div>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-      {/* set modal data up */}
-      <Modal
-        size="lg"
-        show={showModal}
-        onHide={() => setShowModal(false)}
-        aria-labelledby="signup-modal"
-      >
-        {/* tab container to do either signup or login component */}
-        <Tab.Container defaultActiveKey="login">
-          <Modal.Header closeButton>
-            <Modal.Title id="signup-modal">
-              <Nav variant="pills">
-                <Nav.Item className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
-                  <Nav.Link href="/pointsofinterest">
-                    Points of Interest
-                  </Nav.Link>
-                </Nav.Item>
-                <Nav.Item className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
-                  <Nav.Link eventKey="login">Login</Nav.Link>
-                </Nav.Item>
-                <Nav.Item className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
-                  <Nav.Link eventKey="signup">Sign Up</Nav.Link>
-                </Nav.Item>
-              </Nav>
-            </Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <Tab.Content>
-              <Tab.Pane eventKey="login">
-                <LoginForm handleModalClose={() => setShowModal(false)} />
-              </Tab.Pane>
-              <Tab.Pane eventKey="signup">
-                <SignupForm handleModalClose={() => setShowModal(false)} />
-              </Tab.Pane>
-            </Tab.Content>
-          </Modal.Body>
-        </Tab.Container>
-      </Modal>
-    </>
+        </div>
+      </nav>
+      </>
   );
 };
 
